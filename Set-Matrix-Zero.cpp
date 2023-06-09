537aa25e572503f6cf715f7fc2aea04be087664a
Shivam Dixit <dixitshivam4499@gmail.com>
2023-06-09 23:16:37 +0530

    question1

--- Set-Matrix-Zero.cpp
+++ Set-Matrix-Zero.cpp
@@ -4,53 +4,87 @@
 
 
 
-#include <bits/stdc++.h>
-using namespace std;
+// #include <bits/stdc++.h>
+// using namespace std;
 
-void markRow(vector<vector<int>> &matrix,int N,int M,int i)
-    {
-        for(int j=0;j<M;j++)
-        {
-            if(matrix[i][j]!=0)
-            {
-                matrix[i][j]=-1;
-            }
-        }
-    }
-    void markCol(vector<vector<int>> &matrix,int N,int M,int j)
-    {
-        for(int i=0;i<N;i++)
-        {
-            if(matrix[i][j]!=0)
-            {
-                matrix[i][j]=-1;
-            }
-        }
-    }
+// void markRow(vector<vector<int>> &matrix,int N,int M,int i)
+//     {
+//         for(int j=0;j<M;j++)
+//         {
+//             if(matrix[i][j]!=0)
+//             {
+//                 matrix[i][j]=-1;
+//             }
+//         }
+//     }
+//     void markCol(vector<vector<int>> &matrix,int N,int M,int j)
+//     {
+//         for(int i=0;i<N;i++)
+//         {
+//             if(matrix[i][j]!=0)
+//             {
+//                 matrix[i][j]=-1;
+//             }
+//         }
+//     }
+// void setZeros(vector<vector<int>> &matrix)
+// {
+// 	int N=matrix.size();
+//         int M=matrix[0].size();
+//         for(int i=0;i<N;i++)
+//         {
+//             for(int j=0;j<M;j++)
+//             {
+//                 if(matrix[i][j]==0)
+//                 {
+//                     markRow(matrix,N,M,i);
+//                     markCol(matrix,N,M,j);
+//                 }
+//             }
+//         }
+//         for(int i=0;i<N;i++)
+//         {
+//             for(int j=0;j<M;j++)
+//             {
+//                 if(matrix[i][j]==-1)
+//                 {
+//                     matrix[i][j]=0;
+//                 }
+//             }
+//         }
+// }
+
+
+
+// *************************************** Better Solution ***************************************** //
+
+#include<bits/stdc++.h>
+using namespace std;
 void setZeros(vector<vector<int>> &matrix)
 {
-	int N=matrix.size();
-        int M=matrix[0].size();
-        for(int i=0;i<N;i++)
+    int n=matrix.size();
+    int m=matrix[0].size();
+    vector<int> row(n,0);
+    vector<int> col(m,0);
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
         {
-            for(int j=0;j<M;j++)
+            if(matrix[i][j]==0)
             {
-                if(matrix[i][j]==0)
-                {
-                    markRow(matrix,N,M,i);
-                    markCol(matrix,N,M,j);
-                }
+                row[i]=1;
+                col[j]=1;
             }
         }
-        for(int i=0;i<N;i++)
+    }
+    for(int i=0;i<n;i++)
+    {
+        for(int j=0;j<m;j++)
         {
-            for(int j=0;j<M;j++)
+            if(row[i] || col[j])
             {
-                if(matrix[i][j]==-1)
-                {
-                    matrix[i][j]=0;
-                }
+                matrix[i][j]=0;
             }
         }
+    }
 }
-
